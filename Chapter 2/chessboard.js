fa7b2fd6f4fd3988fa4405fe1d8f47bd23764ee3
emTr0 <em@emtr0.com>
2020-05-19 22:18:30 -0700

    Update chessboard.js

--- Chapter 2/chessboard.js
+++ Chapter 2/chessboard.js
@@ -1,2 +1,18 @@
 // Write a program that creates a string that represents an 8x8 grid, using new-line characters to separate lines...
 
+let chessboard = "";
+
+size = 8;
+
+for (let row = 1; row <= size; row++) {
+    for (let chars = 1; chars <= size; chars++) {
+        if ((row + chars) % 2 == 0) {
+            chessboard += " ";
+        } else {
+            chessboard += "#";
+        }
+    }
+    chessboard += "\n";
+}
+
+console.log(chessboard);
