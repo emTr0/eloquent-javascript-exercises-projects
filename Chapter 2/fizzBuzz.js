6da4a6ccef65a8b99e177d43107d0779b1e45f45
emTr0 <em@emtr0.com>
2020-05-19 20:46:49 -0700

    Update fizzBuzz.js

--- Chapter 2/fizzBuzz.js
+++ Chapter 2/fizzBuzz.js
@@ -1,2 +1,16 @@
 // Write a program that uses console.log to print all the numbers from 1 to 100...
 
+let counter = 1;
+
+while (counter <= 100) {
+    if ((counter % 3 == 0) && (counter % 5 == 0)) {
+        console.log("FizzBuzz");
+    } else if (counter % 5 == 0 && !(counter % 3 == 0)) {
+        console.log("Buzz");
+    } else if (counter % 3 == 0) {
+        console.log("Fizz");
+    } else {
+        console.log(counter);
+    }
+    counter++;
+}
